0b08446d0dd5383f3a8975007a8548bd6125e081
leticia.takenaka <leticia.takenaka@BHN050103221>
2023-12-01 15:33:18 -0300

    Exercício 3: Diferença do produto

---
+++ Exercicio_03/Program.cs
@@ -0,0 +1,14 @@
+﻿//Fazer um programa para ler quatro valores inteiros A, B, C e D.
+//A seguir, calcule e mostre a diferença do produto
+//de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D)
+int valor1, valor2, valor3, valor4, diferenca;
+
+Console.WriteLine("Digite 4 valores: " );
+
+valor1 = int.Parse(Console.ReadLine());
+valor2 = int.Parse(Console.ReadLine());
+valor3 = int.Parse(Console.ReadLine());
+valor4 = int.Parse(Console.ReadLine());
+
+diferenca = valor1 * valor2 - valor3 * valor4;
+Console.WriteLine($"DIFERENÇA: {diferenca}");
