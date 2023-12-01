330fbabed6a48d73adce43475656dce7d05106a5
leticia.takenaka <leticia.takenaka@BHN050103221>
2023-12-01 15:40:02 -0300

    Exercício 4: Cálculo de salário

---
+++ Exercicio_04/Program.cs
@@ -0,0 +1,17 @@
+﻿//Fazer um programa que leia o número de um funcionário
+//seu número de horas trabalhadas,
+//o valor que recebe por hora e
+//calcule o salário desse funcionário. A seguir,
+//mostre o número e o salário do funcionário, com duas casas decimais.
+
+Console.Write("Digite o número do funcionário: ");
+string numeroFuncionario = Console.ReadLine();
+
+Console.Write("Digite o número de horas trabalhadas: ");
+float horasTrabalhadas = float.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor recebido por hora: ");
+float valorRecebidoPorHora = float.Parse(Console.ReadLine());
+
+double salario = (double) horasTrabalhadas * valorRecebidoPorHora;
+Console.WriteLine($"O salário do funcionário é R$ {salario.ToString("F2")}");
