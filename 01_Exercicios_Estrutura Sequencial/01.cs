ab2eebd5732fbecb31f7529d8ad8c352a3f8af83
leticia.takenaka <leticia.takenaka@BHN050103221>
2023-12-01 14:58:49 -0300

    Adicionar arquivos de projeto.

---
+++ 01_Exercicios_Estrutura Sequencial/01.cs
@@ -0,0 +1,15 @@
+﻿//Faça um programa para ler dois valores inteiros, e 
+//depois mostrar na tela a soma desses números com uma
+//mensagem explicativa
+
+int valor1, valor2, somaValores;
+
+Console.WriteLine("Digite um valor: ");
+valor1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro valor: ");
+valor2 = int.Parse(Console.ReadLine());
+
+somaValores = valor1 + valor2;
+
+Console.WriteLine($"A soma de {valor1} + {valor2} é igual à {somaValores}");
